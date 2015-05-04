68f8715bd5d74175587ab509e06b74b0981928e6
Chris Rogers <chrxr@hotmail.co.uk>
2015-05-04 12:08:00 +0100

    added allowed hosts

--- wagans/settings/production.py
+++ wagans/settings/production.py
@@ -5,7 +5,7 @@ from .base import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-
+ALLOWED_HOSTS = ['skatedev.com', 'www.skatedev.com','178.62.92.190',]
 
 # Compress static files offline
 # http://django-compressor.readthedocs.org/en/latest/settings/#django.conf.settings.COMPRESS_OFFLINE
