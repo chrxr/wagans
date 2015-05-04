3e671887a55a9794dc6e462bbd93d150a5c02c01
Chris Rogers <chrxr@hotmail.co.uk>
2015-05-04 12:14:19 +0100

    added allowed hosts

--- wagans/settings/base.py
+++ wagans/settings/base.py
@@ -21,9 +21,9 @@ PROJECT_ROOT = dirname(dirname(dirname(abspath(__file__))))
 SECRET_KEY = '@$@^9jiirc5en2h@a@cs*))pojqrg1t+2_c22&6h+al-am366o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['skatedev.com', 'www.skatedev.com','178.62.92.190']
 
--- wagans/wsgi.py
+++ wagans/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagans.settings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagans.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
